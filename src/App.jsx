02d13522749d0f79b93305461cdb89554ea1d4f0
Vivek Singh <viveksingh9301@gmail.com>
2025-03-22 14:42:46 +0530

    Add Main component and show navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
+import Main from "./components/Main/Main.jsx";
 
 const App = () => {
   return (
-      <main>
+      <main className="flex flex-nowrap">
         <Sidebar/>
+        <Main/>
       </main>
   )
 };
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {assets} from "../../assets/assets.js";
+
+function Main() {
+  return (
+      <div className="w-full p-4">
+        <nav className="flex justify-between">
+          <p className="text-2xl text-[#585858]">Gemini</p>
+          <img src={assets.user_icon} width="40px" className="rounded-full" alt="user icon"/>
+        </nav>
+      </div>
+  );
+}
+
+export default Main;
