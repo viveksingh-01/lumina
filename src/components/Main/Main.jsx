84342a54a89c6c289cc29330b9b01c2835bfdd03
Vivek Singh <viveksingh9301@gmail.com>
2025-03-23 00:26:16 +0530

    Add gradient text styling for 'Lumina' in Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { assets } from "../../assets/assets.js";
+import "./Main.css";
 
 function Main() {
   return (
@@ -13,9 +14,11 @@ function Main() {
           alt="user icon"
         />
       </nav>
-      <section className="h-60 flex justify-center items-center">
-        <div className="mx-auto text-5xl font-bold text-center text-[#666]">
-          <h3>Meet Lumina,</h3>
+      <section className="h-120 flex justify-center items-center">
+        <div className="mx-auto text-5xl font-bold text-center text-[#c4c7c5]">
+          <h3>
+            Meet <span className="lumina-text">Lumina</span>,
+          </h3>
           <h3>your personal AI assistant.</h3>
         </div>
       </section>
