675c29b2d35f1577831e87cb6e2b8d477a3b8091
Vivek Singh <viveksingh9301@gmail.com>
2025-03-23 00:10:44 +0530

    Enhance Main component layout with a welcome section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import {assets} from "../../assets/assets.js";
+import React from "react";
+import { assets } from "../../assets/assets.js";
 
 function Main() {
   return (
-      <div className="w-full p-4">
-        <nav className="flex justify-between">
-          <p className="text-2xl text-[#585858]">Lumina</p>
-          <img src={assets.user_icon} width="40px" className="rounded-full" alt="user icon"/>
-        </nav>
-      </div>
+    <div className="w-full p-5 px-7">
+      <nav className="flex justify-between">
+        <p className="text-2xl text-[#585858]">Lumina</p>
+        <img
+          src={assets.user_icon}
+          width="40px"
+          className="rounded-full"
+          alt="user icon"
+        />
+      </nav>
+      <section className="h-60 flex justify-center items-center">
+        <div className="mx-auto text-5xl font-bold text-center text-[#666]">
+          <h3>Meet Lumina,</h3>
+          <h3>your personal AI assistant.</h3>
+        </div>
+      </section>
+    </div>
   );
 }
 
