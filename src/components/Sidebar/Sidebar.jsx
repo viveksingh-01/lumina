bc0619e526c44f2a39f2878e1dfea9ef0c2358cd
Vivek Singh <viveksingh9301@gmail.com>
2025-04-07 23:57:45 +0530

    feat: add sidebar toggle functionality to show/hide recent prompts

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import { assets } from "/src/assets/assets.js";
 
 function Sidebar({ promptHistory, setCurrentPrompt }) {
+  const [sidebarToggler, setSidebarToggler] = useState(true);
+
+  function handleSidebarToggle() {
+    setSidebarToggler((value) => !value);
+  }
+
   return (
     <aside className="p-4 min-h-screen inline-flex flex-col justify-between bg-[#f0f4f9]">
       <div className="top">
         <img
+          onClick={handleSidebarToggle}
           src={assets.menu_icon}
           className="cursor-pointer"
           alt="menu icon"
         />
-        {promptHistory.length > 0 && (
+        {sidebarToggler && promptHistory.length > 0 && (
           <div className="my-5 flex flex-col">
             <p className="mb-2">Recent</p>
             {promptHistory.map((prompt, index) => (
