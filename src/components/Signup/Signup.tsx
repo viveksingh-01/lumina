b11f6c8b2ca457b5d709fd48e9b0beb0bbedcc10
Vivek Singh <viveksingh9301@gmail.com>
2025-07-08 23:00:48 +0530

    feat: add Signup component for user registration

---
+++ src/components/Signup/Signup.tsx
@@ -0,0 +1,14 @@
+const Signup: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center pt-20 bg-gray-50 p-4">
+      <h1 className="text-3xl p-2 mb-9">Create an account</h1>
+      <div>
+        <form>
+          <div>Signup Form</div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
