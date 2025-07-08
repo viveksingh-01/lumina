3bbf731c65cc019b6c66db6cad9c3b8694166f9c
Vivek Singh <viveksingh9301@gmail.com>
2025-07-08 23:02:17 +0530

    feat: add Auth component and update routing in App for user authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from "react-router-dom";
+import Signup from "./components/Signup/Signup.js";
+import Auth from "./pages/Auth/Auth.js";
 import Home from "./pages/Home/Home.js";
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" Component={Home} />
+      <Route path="auth" Component={Auth}>
+        <Route path="signup" Component={Signup} />;
+      </Route>
     </Routes>
   );
 };
---
+++ src/pages/Auth/Auth.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+const Auth: React.FC = () => {
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
+};
+
+export default Auth;
