c4a06defe0c597f89c21674975ac374427be3709
Vivek Singh <viveksingh9301@gmail.com>
2025-07-09 19:19:13 +0530

    feat: implement Signup component with email and password input fields

--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import InputField from "../InputField/InputField";
+
 const Signup: React.FC = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
     <div className="min-h-screen flex flex-col items-center pt-20 bg-gray-50 p-4">
       <h1 className="text-3xl p-2 mb-9">Create an account</h1>
       <div>
         <form>
-          <div>Signup Form</div>
+          <InputField type="email" name="email" placeholder="Email" value={formData.email} />
+          <InputField type="password" name="password" placeholder="Password" value={formData.password} />
         </form>
       </div>
     </div>
