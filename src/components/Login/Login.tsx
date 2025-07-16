4e30da2c6de620db96d9c71f124e2d55b634a3f6
Vivek Singh <viveksingh9301@gmail.com>
2025-07-16 21:34:14 +0530

    feat: add email and password fields in Login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { ILoginFormData } from "../../types/form-data";
+import InputField from "../InputField/InputField";
+
 const Login: React.FC = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState<ILoginFormData>({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <div className="min-h-screen flex flex-col items-center pt-20 bg-gray-50 p-4">
+      <h1 className="text-3xl p-2 mb-9">Welcome back!</h1>
+      <div>
+        <form>
+          <InputField
+            type="email"
+            name="email"
+            placeholder="Email address"
+            value={formData.email}
+            setValue={setFormData}
+            autoFocus={true}
+          />
+          <InputField
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            setValue={setFormData}
+            autoFocus={false}
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
