e438dea98a5d3d8911724ecdb986dad37b4e1143
Vivek Singh <viveksingh9301@gmail.com>
2025-07-29 21:03:19 +0530

    chore: set withCredentials property while making POST request for user registration
    - to enable setting of JWT as cookie

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,6 +4,6 @@ import { ISignupRequestPayload } from "../types/request-payload";
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function createAccount(body: ISignupRequestPayload) {
-  const response = await axios.post(`${BASE_URL}/register`, body);
+  const response = await axios.post(`${BASE_URL}/register`, body, { withCredentials: true });
   return response?.data;
 }
