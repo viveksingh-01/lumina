60f7aa494915a4ae5deee1105c89b578df731fa7
Vivek Singh <viveksingh9301@gmail.com>
2025-07-30 19:23:34 +0530

    wip: update success-response interface to include user details

--- src/types/response.ts
+++ src/types/response.ts
@@ -1,5 +1,11 @@
 export interface ISuccessResponse {
+  success: boolean;
   message: string;
+  data: {
+    id: string;
+    email: string;
+    name: string;
+  };
 }
 
 export interface IErrorResponse {
