77ad6055fc8779553d33ebfbf264f525f17fb1fb
Vivek Singh <viveksingh9301@gmail.com>
2025-08-03 00:23:02 +0530

    feat: add login function to handle user authentication

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
+import { ILoginFormData } from "../types/form-data";
 import { ISignupRequestPayload } from "../types/request-payload";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function createAccount(body: ISignupRequestPayload) {
   const response = await axios.post(`${BASE_URL}/register`, body, { withCredentials: true });
+  return response?.data;
+}
+
+export async function login(payload: ILoginFormData) {
+  const response = await axios.post(`${BASE_URL}/login`, payload, { withCredentials: true });
   return response?.data;
 }
