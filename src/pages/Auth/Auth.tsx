cc106b839d8467cfc230cb445d3e110d219befad
Vivek Singh <viveksingh9301@gmail.com>
2025-08-06 20:14:34 +0530

    feat: navigate to home page on click brand-header in login and signup pages

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,10 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Auth: React.FC = () => {
   return (
     <main className="min-h-screen bg-gray-50">
       <nav className="p-5 ml-[88px] mb-8">
-        <p className="text-2xl text-[#585858]">Lumina</p>
+        <Link to="/">
+          <h2 className="text-2xl text-[#585858]">Lumina</h2>
+        </Link>
       </nav>
       <Outlet />
     </main>
