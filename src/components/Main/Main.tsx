4e05c79d9c8ebf268ece1977c4bab79caabd0353
Vivek Singh <viveksingh9301@gmail.com>
2025-08-07 10:38:19 +0530

    feat: add navigation link for Signup page in the navbar

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -49,11 +49,18 @@ const Main: React.FC<MainProps> = ({ currentPrompt, promptHistory, setPromptHist
     <div className="w-full p-5 px-7">
       <nav className="flex justify-between items-center pr-2">
         <p className="text-2xl text-[#585858]">Lumina</p>
-        <Link to="/auth/log-in">
-          <span className="py-2 px-4 rounded-full text-sm tracking-wide text-gray-50 bg-gray-900 hover:bg-gray-800 transition-colors">
-            Log in
-          </span>
-        </Link>
+        <div>
+          <Link to="/auth/log-in">
+            <span className="py-2 px-4 rounded-full text-sm tracking-wide text-gray-50 bg-gray-900 hover:bg-gray-800 transition-colors">
+              Log in
+            </span>
+          </Link>
+          <Link to="/auth/create-account">
+            <span className="ml-4 py-2 px-4 rounded-full text-sm tracking-wide bg-[#f0f4f9] hover:bg-gray-200 transition">
+              Sign up for free
+            </span>
+          </Link>
+        </div>
       </nav>
       <div className="h-[calc(100vh-80px)] flex flex-col">
         <section className="grow flex justify-center items-center overflow-y-auto">
