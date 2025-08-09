dc874a55e9cf2b4901d2440d61219c92354aa0d5
Vivek Singh <viveksingh9301@gmail.com>
2025-08-09 12:50:49 +0530

    feat: display user's name on navbar upon successful authorization

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
 const Navbar: React.FC = () => {
+  const { isAuthorized, user } = useAuth();
+  let firstName: string = "";
+  if (user?.name) {
+    firstName = user.name.split(" ")[0];
+  }
+
   return (
     <nav className="flex justify-between items-center pr-2">
       <p className="text-2xl text-[#585858]">Lumina</p>
-      <div>
-        <Link to="/auth/log-in">
-          <span className="py-2 px-4 rounded-full text-sm tracking-wide text-gray-50 bg-gray-900 hover:bg-gray-800 transition-colors">
-            Log in
-          </span>
-        </Link>
-        <Link to="/auth/create-account">
-          <span className="ml-4 py-2 px-4 rounded-full text-sm tracking-wide bg-[#f0f4f9] hover:bg-gray-200 transition">
-            Sign up for free
-          </span>
-        </Link>
-      </div>
+      {isAuthorized ? (
+        <p>{firstName}</p>
+      ) : (
+        <div>
+          <Link to="/auth/log-in">
+            <span className="py-2 px-4 rounded-full text-sm tracking-wide text-gray-50 bg-gray-900 hover:bg-gray-800 transition-colors">
+              Log in
+            </span>
+          </Link>
+          <Link to="/auth/create-account">
+            <span className="ml-4 py-2 px-4 rounded-full text-sm tracking-wide bg-[#f0f4f9] hover:bg-gray-200 transition">
+              Sign up for free
+            </span>
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
