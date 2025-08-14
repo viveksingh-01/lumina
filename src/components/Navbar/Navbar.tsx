14fbdb8549f761d4633099f91b5cbd499b4a46a1
Vivek Singh <viveksingh9301@gmail.com>
2025-08-14 23:59:45 +0530

    wip: add dropdown for logout option in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,10 @@
-import { User } from "lucide-react";
-import React from "react";
+import { UserCircle2 } from "lucide-react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 
 const Navbar: React.FC = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const { isAuthorized, user } = useAuth();
   let firstName: string = "";
   if (user?.name) {
@@ -14,9 +15,16 @@ const Navbar: React.FC = () => {
     <nav className="flex justify-between items-center pr-2">
       <p className="text-2xl text-[#585858]">Lumina</p>
       {isAuthorized ? (
-        <div className="flex gap-1 items-center">
-          <User size={20} strokeWidth={1.25} />
+        <div className="flex gap-1.5 items-center">
           <p className="text-lg tracking-wide text-[#1b1c1d]">{firstName}</p>
+          <div onClick={() => setIsDropdownOpen(!isDropdownOpen)} className="relative inline-block">
+            <UserCircle2 size={28} strokeWidth={1.25} />
+          </div>
+          {isDropdownOpen && (
+            <div className="absolute w-36 top-14 right-9 z-10 px-4 py-2 text-sm tracking-wide text-gray-700 hover:bg-gray-100 hover:cursor-pointer rounded-md bg-white shadow-lg ring-1 ring-[#1b1c1d] ring-opacity-5">
+              Sign out
+            </div>
+          )}
         </div>
       ) : (
         <div>
