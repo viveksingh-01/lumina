46e4174b45840646204a2bbf2edb537f649b7b95
Vivek Singh <viveksingh9301@gmail.com>
2025-08-31 19:15:53 +0530

    style: modify styles for the login-alert popup

--- src/components/LoginAlert/LoginAlert.tsx
+++ src/components/LoginAlert/LoginAlert.tsx
@@ -7,19 +7,22 @@ const LoginAlert: React.FC<ILoginAlertProps> = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-50">
-      <div className="bg-white rounded-lg shadow-lg p-6 w-96">
-        <h2 className="text-2xl font-semibold text-center mb-4">Login Required</h2>
-        <p className="text-center mb-4">Please login to use Lumina.</p>
-        <div className="flex justify-center space-x-4">
-          <button
-            onClick={onClose}
-            className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400 transition"
-          >
-            Cancel
-          </button>
+      <div className="relative bg-white rounded-lg shadow-lg p-6 w-[360px]">
+        <button
+          onClick={onClose}
+          className="absolute py-2 px-4 top-2 right-2 text-2xl text-gray-500 hover:text-gray-700 hover:cursor-pointer hover:bg-gray-100 rounded-full"
+          aria-label="Close"
+        >
+          &times;
+        </button>
+
+        <h2 className="text-lg text-center text-sky-700 mb-6">Login Required</h2>
+        <p className="text-center mb-6">Please login to use Lumina.</p>
+
+        <div className="flex justify-center">
           <button
             onClick={() => alert("Login Alert!")}
-            className="bg-sky-600 text-white px-4 py-2 rounded-md hover:bg-sky-700 transition"
+            className="mb-2 py-3 px-8 rounded-full text-sm tracking-wide text-gray-50 bg-gray-900 hover:bg-gray-800 hover:cursor-pointer transition"
           >
             Login
           </button>
