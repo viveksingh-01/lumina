59b9576a4fd0c9723752af8c409acf03bbcc3163
Vivek Singh <viveksingh9301@gmail.com>
2025-09-03 20:34:44 +0530

    feat: add Shimmer component to show loading effects

---
+++ src/components/Shimmer/Shimmer.tsx
@@ -0,0 +1,5 @@
+const Shimmer: React.FC<{ width?: string }> = ({ width = "w-full" }) => {
+  return <div className={`h-96 ${width} bg-gray-200 rounded animate-pulse`}></div>;
+};
+
+export default Shimmer;
